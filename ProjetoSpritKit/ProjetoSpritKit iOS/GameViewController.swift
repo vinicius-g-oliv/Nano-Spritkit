b174c9b5428496c9f04c0b070db4d2305a7d3b7b
Vinicius Gomes <78090044+vinicius-g-oliv@users.noreply.github.com>
2022-11-03 17:14:26 -0300

    Initial commit

---
+++ ProjetoSpritKit/ProjetoSpritKit iOS/GameViewController.swift
@@ -0,0 +1,39 @@
+//
+//  GameViewController.swift
+//  ProjetoSpritKit iOS
+//
+//  Created by Vinicius Gomes on 03/11/22.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let scene = GameScene.newGameScene()
+
+        // Present the scene
+        let skView = self.view as! SKView
+        skView.presentScene(scene)
+        
+        skView.ignoresSiblingOrder = true
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
+        } else {
+            return .all
+        }
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
